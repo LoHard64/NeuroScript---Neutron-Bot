f242ec6eae4a2eb71d47c4ba92f506ef2a8d02d7
LoHard64 <lohardcraft64@gmail.com>
2023-02-26 09:52:14 +0700

    add dalle command

--- plugins/dalle.js
+++ plugins/dalle.js
@@ -1,24 +1,26 @@
 import { Configuration, OpenAIApi } from "openai";
-let handler = async (m, { conn, text, chatUpdate }) => {
+let handler = async (m, { conn, text, usedPrefix, command, chatUpdate }) => {
+  const from = m.key.remoteJid;
+  const reply = m.reply;
   if (!text)
-    return reply(
-      `Membuat gambar dari AI.\n\nContoh:\n${prefix}${command} Wooden house on snow mountain`
+    return m.reply(
+      `Membuat gambar dari AI.\n\nContoh:\n${usedPrefix}${command} Wooden house on snow mountain`
     );
   const configuration = new Configuration({
     apiKey: "",
   });
-  const from = m.chat;
-  const mek = chatUpdate.messages[0];
+
   const openai = new OpenAIApi(configuration);
   const response = await openai.createImage({
     prompt: text,
     n: 1,
     size: "512x512",
   });
+  // console.log(m.key.remoteJid);
   //console.log(response.data.data[0].url)
-  m.reply(from, response.data.data[0].url, text, mek);
+  conn.sendFile(from, response.data.data[0].url, reply, text, m);
 };
-handler.help = ["img", "ai-img", "image"];
+handler.help = ["img", "ai-img"];
 handler.tags = ["info", "fun"];
 handler.command = /^(img|aiimg|image)$/i;
 export default handler;
